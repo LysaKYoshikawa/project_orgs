852ff3ea3bacb3e63f24d341c4312d806a2b343a
Monalysa <monalysaklauck@gmail.com>
2022-09-25 02:59:56 -0300

    add-button-floating

--- app/src/main/java/com/yoshikawa/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/yoshikawa/orgs/ui/activity/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.yoshikawa.orgs.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yoshikawa.orgs.R
 import com.yoshikawa.orgs.model.Produto
 import com.yoshikawa.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
@@ -14,12 +16,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de Frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Maça, Laranja e uva"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
+
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
@@ -30,8 +27,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             Produto(nome = "Frutas",
                     descricao = "Laranja",
                     valor = BigDecimal("15.60")
-            )
+            ),
         ))
-        //recyclerView.layoutManager = LinearLayoutManager(this)
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            var intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
